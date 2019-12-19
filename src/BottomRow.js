7ae465a12d1af40a56c555f6fe1071d73f8f858f
MLucky518 <lucky32815@gmail.com>
2019-12-18 21:31:22 -0500

    done i swear

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -5,27 +5,44 @@ const BottomRow = () => {
 
   
   const[quarter,setQuarter] = useState(0);
+  const [down,setDown] = useState(0);
+  const [toGo,setToGo] = useState(0);
+  const [ballOn,setBallOn] = useState(0);
 
+  const handleRandomGame = (e)=>{
+  setQuarter(Math.ceil(Math.random() * 4));
+  setDown( Math.ceil(Math.random()* 4));
+  setBallOn(Math.ceil(Math.random() * 50));
+  setToGo(Math.ceil(Math.random()*25));
+
+  }
 
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">4</div>
+        <div className="down__value">{down}</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">1</div>
+        <div className="toGo__value">{toGo}</div>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">1</div>
+        <div className="ballOn__value">{ballOn}</div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{quarter}</div>
+        
+      </div>
+     
+      <div>
+      <button onClick = {handleRandomGame}  className="homeButtons__touchdown" className = "random">randomize game conditions</button>
       </div>
     </div>
+    
+    
   );
 };
 
